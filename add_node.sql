b38d8d8d5dfa684a64b6dcc63500a75c75ddd032
Alex <seobots@gmail.com>
2013-04-07 04:44:24 +0300

    sql

--- add_node.sql
+++ add_node.sql
@@ -1,3 +1,3 @@
 SET @name = 'node';
 SET @parent = '1';
-insert into node (`name`,`parent`,`inheritance`) values (@name, @parent, (select concat(inheritance,'|', sum) from (select inheritance, (select (`id_node` +1) from node as n ORDER BY `n`.`id_node` DESC LIMIT 1) as sum from node where id_node = @parent) as tmp limit 1));
+insert into node (`name`,`parent`,`inheritance`) values (@name, @parent, (select IFNULL((select concat(inheritance,'|', sum) from (select inheritance, (select (`id_node` +1) from node as n ORDER BY `n`.`id_node` DESC LIMIT 1) as sum from node where id_node = @parent) as tmp limit 1), (select (`id_node` +1) from node as n ORDER BY `n`.`id_node` DESC LIMIT 1))));
